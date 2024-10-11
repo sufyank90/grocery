27286fa3d316a8acd9ba4a600fc0d39fb412356d
Huzaifa11193 <99352212+Huzaifa11193@users.noreply.github.com>
2024-10-11 07:32:36 -0700

    update

--- resources/js/Pages/order/View.jsx
+++ resources/js/Pages/order/View.jsx
@@ -45,7 +45,7 @@ const View = (props) => {
                         <div className="text-right">
                             <p>Air Express Mart</p>
                             <p className="text-gray-500 text-sm">sales@airexpressmart.com</p>
-                            <p className="text-gray-500 text-sm mt-1">+92-000000000</p>
+                            <p className="text-gray-500 text-sm mt-1">+92-3331325935</p>
                             {/* <p className="text-gray-500 text-sm mt-1">VAT: 8657671212</p> */}
                         </div>
                     </div>
