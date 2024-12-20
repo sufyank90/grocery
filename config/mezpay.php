77c6cdedb0928da01ca541088c17ed58b0e4914a
danishxp <danish.khalid@solvixs.com>
2024-12-20 18:16:13 +0500

    ok

--- config/mezpay.php
+++ config/mezpay.php
@@ -3,7 +3,7 @@
 return [
     'username' => env('MEZPAY_USERNAME', ''),
     'password' => env('MEZPAY_PASSWORD', ''),
-    'success_callback' => env('MEZPAY_SUCCESS_CALLBACK', 'success'),
-    'failed_callback' => env('MEZPAY_FAILED_CALLBACK', 'failed'),
+    'success_callback' => env('MEZPAY_SUCCESS_CALLBACK', env('APP_URL') . '/order_payment/success'),
+    'failed_callback' => env('MEZPAY_FAILED_CALLBACK', env('APP_URL') . '/order_payment/failed'),
 ];
 
