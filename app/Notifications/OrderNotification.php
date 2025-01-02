50c4032c9d0e3c67d88de2bd102bb81278c400a1
sufyantechnoties <sufyan.technoties@gmail.com>
2025-01-02 15:29:37 +0500

    update

--- app/Notifications/OrderNotification.php
+++ app/Notifications/OrderNotification.php
@@ -32,7 +32,11 @@ class OrderNotification extends Notification
      */
     public function via($notifiable)
     {
-        return ['database'];
+        // Only send notification via database if order status is "pending"
+        if ($this->order->status === 'pending') {
+            return ['database'];
+        }
+        return [];
     }
 
     /**
